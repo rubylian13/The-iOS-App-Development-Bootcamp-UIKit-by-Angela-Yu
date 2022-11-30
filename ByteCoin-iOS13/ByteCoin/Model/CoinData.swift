b273e7045ae05ead0766ea8aba7ab636ef102ce0
rubylian <rubylian5813@gmail.com>
2022-11-30 10:49:49 +0800

    Add project: ByteCoin

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ruby Lien on 2022/11/25.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
